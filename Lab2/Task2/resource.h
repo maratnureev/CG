cf1f9b85a6584676af8eabf22b251f62385849b0
NiCrOmAnT <61577550+NiCrOmAnT@users.noreply.github.com>
2022-03-15 18:13:45 +0300

    Сделал 2 лабу

--- Lab2/Task2/resource.h
+++ Lab2/Task2/resource.h
@@ -1,25 +1,22 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Включаемый файл, созданный в Microsoft Visual C++.
-// Используется Lab2.rc
+// Используется Task2.rc
 //
 #define IDC_MYICON                      2
-#define IDD_LAB2_DIALOG                 102
+#define IDD_TASK2_DIALOG                102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
 #define IDM_EXIT                        105
-#define IDI_LAB2                        107
+#define IDI_TASK2                       107
 #define IDI_SMALL                       108
-#define IDC_LAB2                        109
+#define IDC_TASK2                       109
 #define IDR_MAINFRAME                   128
-#define IDR_MENU1                       129
 #define IDR_MAIN_MENU                   129
-#define IDR_ACCELERATOR1                130
 #define ID_FILE_OPEN                    32771
 #define ID_FILE_SAVE                    32772
 #define ID_FILE_EXIT                    32773
-#define MAIN                            32774
-#define ID_ACCELERATOR32775             32775
+#define ID_FILE_NEW                     32774
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -28,7 +25,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_COMMAND_VALUE         32775
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
